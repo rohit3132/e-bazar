d35ef36221937dd18ca83a357925c7b991cfa9f5
rohit3132 <rohitlaskar95@gmail.com>
2023-09-02 13:28:44 +0530

    productCategoryedit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Category from './components/Category';
+
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
+React.useEffect(() => {
+fetch("http://localhost:3001/categories")
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+  setCategories(data);
+})
+}, [])
+
+const handleCategoryClick = id =>{
+fetch("http://localhost:3001/products?catId="+id)
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+  setProducts(data);
+})
+}
+
+const renderCatgories = () => {
+  return categories.map(c => 
+  <Category key={c.id} id={c.id} title={c.title} onCategoryClick={() => handleCategoryClick(c.id)}/>
+  );
+}
+
+const renderProducts = () => {
+  return products.map(p =>
+    <div>{p.title}</div>
+    )
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header> e-bazar </header>
+
+    <section>
+      <nav> 
+       { categories && renderCatgories() } 
+      </nav>
+      <article>
+        <h1>Products</h1>
+        { products && renderProducts()}
+      </article>
+    </section>
+
+<footer>
+  footer
+</footer>
+    </>
   );
 }
 
