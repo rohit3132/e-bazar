e3a23659ab7b202ff7599efc8481f0e632265667
rohit3132 <rohitlaskar95@gmail.com>
2023-09-08 02:30:14 +0530

    editproductdetail

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ProductDetail from './components/productDetails';
+import Basket from './components/basket';
+import Checkout from './components/checkout';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path ="/" elements={<App/>} />
+      <Route path="basket" element={<Basket/>} /> 
+      <Route path ="checkout" element ={<Checkout />} />
+      <Route path="products/:productId" element ={<ProductDetail />} />
+    </Routes>
+
+    
+    </BrowserRouter>
   </React.StrictMode>
 );
 
